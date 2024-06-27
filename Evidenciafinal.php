c6d10dd3beb232371821218c997d009d738ad51b
MaryoriRojas <maryorirojas2004@gmail.com>
2024-06-27 18:39:44 -0500

    solicitud de informacion

--- Evidenciafinal.php
+++ Evidenciafinal.php
@@ -4,3 +4,21 @@ echo "ingrese el total de empleados\n";
 $total_empleados = intval(readline());
 $empelados = [];
 
+for ($i = 1; $i = $total_empleados; $i++) {
+    echo "empelado $i\n";
+    echo "ingrese su nombre\n";
+    $nombre = readline();
+    echo "ingrese su identidad \n";
+    $dni =intval(readline());
+    echo "ingrese su genero \n";
+    $genero = readline();
+    echo "ingrese su edad \n";
+    $edad = (readline);
+    echo "ingrese su estatura \n";
+    $estatura = floatval(readline());
+    echo "ingrese su peso \n";
+    $peso = floatval(readline);
+
+
+}
+
